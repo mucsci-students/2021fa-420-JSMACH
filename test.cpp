e3f73f7421792d15bd4b98fa18cfe1251566910c
manwellaa <63563468+manwellaa@users.noreply.github.com>
2021-09-06 16:43:17 -0400

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    
+    cout << "Please Enter Number of classes: ";
+    cin >> N;
+    
+    vector<string> store(N);
+    
+    for (int i = 0; i < N; i++)
+    {
+        cout << "Class #" << i+1 << " ";
+        cin >> store[i];
+    }
+    
+    cout << "\n\nHere are the classes:\n";
+    for (size_t i=0; i<store.size(); ++i)
+    {
+        cout << store[i] << '\t';
+    }
+    
+    return 0;
+}
