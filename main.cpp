178b34abeae1feff38919bc14bfe0f0bf5345d76
manwellaa <63563468+manwellaa@users.noreply.github.com>
2021-09-06 20:19:01 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,12 +3,35 @@
 
 using namespace std;
 
+struct  ClassFeilds{
+    std::string feilds;
+};
+struct ClassName{
+    std::string  classname;
+};
+
+struct classMethods{
+    std::string methods;
+};
+
+struct Relationships {
+     std::string name;              // shop chain has-a name
+     std::vector<ClassName> Relationships;       // and shops
+    
+};
+
 int main()
 {
     int N;
     
     cout << "Please Enter Number of classes: ";
     cin >> N;
+    
+    
+    //vector<vector<vector<vector<int>>>> vec(100, vector<vector<vector<int>>>
+    //                                       (200, vector<vector<int>>
+    //                                       (100, vector<int>
+    //                                       (50))));
     
     vector<string> store(N);
     
@@ -23,6 +46,8 @@ int main()
     {
         cout << store[i] << '\t';
     }
+    
+    
     
     return 0;
 }
