8bfc689344727c153ee995adeb5ad8be42256c48
manwellaa <63563468+manwellaa@users.noreply.github.com>
2021-09-07 18:31:44 -0700

    Update UMLRelationship.cpp
    Updated setters and getters

--- UMLRelationship.cpp
+++ UMLRelationship.cpp
@@ -1,1 +1,19 @@
 #include "UMLRelationship.h"
+#include <string>
+
+UMLRelationship::UMLRelationship(std::string name, UMLClass& src, UMLClass& dest){
+  this->RelationshipName=name;
+  this->ClassSrc= src;
+  this->ClassDest=dest;
+}
+
+auto UMLRelationship::get_relationship_name(){
+  return this->RelationshipName;
+}
+auto UMLRelationship::get_src_class(){
+  return this->ClassSrc;
+}
+auto UMLRelationship:: get_dest_class(){
+  return this->ClassDest;
+}
+  
