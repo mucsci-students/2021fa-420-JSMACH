47883854747d167dbe7d8364f770e51610586233
manwellaa <63563468+manwellaa@users.noreply.github.com>
2021-09-07 18:35:57 -0700

    Updatemain (#13)
    * Update main.cpp
    
    * Update UMLAttribute.cpp
    
    Updated setters and getters
    
    * Update UMLRelationship.cpp
    
    Updated setters and getters
    
    * Update main.cpp

--- UMLAttribute.cpp
+++ UMLAttribute.cpp
@@ -1,1 +1,15 @@
 #include "UMLAttribute.h"
+#include <string>
+#include "UMLClass.h"
+
+UMLAttribute::UMLAttribute(std::string name) {
+  this->AttributeName= name;
+}
+
+std::string UMLAttribute::get_attribute_name(){
+  return this->AttributeName;
+}
+
+void set_attribute_name(std::string newName){
+   this->AttributeName= name;
+}
--- main.cpp
+++ main.cpp
@@ -1,50 +1,31 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
 
-struct  ClassFeilds{
-    std::string feilds;
-};
-struct ClassName{
-    std::string  classname;
-};
-
-struct classMethods{
-    std::string methods;
-};
-
-struct Relationships {
-     std::string name;              // shop chain has-a name
-     std::vector<ClassName> Relationships;       // and shops
     
-};
+
 
 int main()
 {
     int N;
     
-    cout << "Please Enter Number of classes: ";
-    cin >> N;
+    std::cout << "Please Enter Number of classes: ";
+    std::cin >> N;
     
+
     
-    //vector<vector<vector<vector<int>>>> vec(100, vector<vector<vector<int>>>
-    //                                       (200, vector<vector<int>>
-    //                                       (100, vector<int>
-    //                                       (50))));
-    
-    vector<string> store(N);
+    std::vector<string> store(N);
     
     for (int i = 0; i < N; i++)
     {
-        cout << "Class #" << i+1 << " ";
-        cin >> store[i];
+        std::cout << "Class #" << i+1 << " ";
+        std::cin >> store[i];
     }
     
-    cout << "\n\nHere are the classes:\n";
+    std::cout << "\n\nHere are the classes:\n";
     for (size_t i=0; i<store.size(); ++i)
     {
-        cout << store[i] << '\t';
+        std::cout << store[i] << '\t';
     }
     
     
