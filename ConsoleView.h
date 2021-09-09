25d4ac3b9b9104c7acf3c0f66310c3c6fd18acff
manwellaa <63563468+manwellaa@users.noreply.github.com>
2021-09-08 22:34:33 -0700

    Update ConsoleView.h
    basic feilds

--- ConsoleView.h
+++ ConsoleView.h
@@ -5,5 +5,15 @@
     The ConsoleView is responsible for displaying information to the user.
     Very little if any application logic is handled in the view class.
 */
+class ConsoleView
+{
+private:
+    std::string input1;
+    std::string input2:
+public:
+    UserInput(std::string input1, std::string input2);
+    ~UserInput();
+};
+
 
 #endif
