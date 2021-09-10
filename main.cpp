eb5129cf229e82aff416a547376ea76025c88bdd
manwellaa <63563468+manwellaa@users.noreply.github.com>
2021-09-09 18:28:04 -0700

    Update main.cpp
    Moved logic out of main

--- main.cpp
+++ main.cpp
@@ -2,33 +2,13 @@
 #include <vector>
 #include <string>
 #include "UMLClass.h"
+#include "ConsoleView.h"
 
 
 int main()
 {
-    int N;
-    
-    std::cout << "Please Enter Number of classes: ";
-    std::cin >> N;
-    
-
-    
-    std::vector<std::string>  store(N);
-    
-    for (int i = 0; i < N; i++)
-    {
-        std::cout << "Class #" << i+1 << " ";
-        std::cin >> store[i];
-    }
-    
-    std::cout << "\n\nHere are the classes:\n";
-    for (size_t i=0; i<store.size(); ++i)
-    {
-        std::cout << store[i] << '\t';
-    }
-    
-    
-    
-    return 0;
+   new ConsoleView= view;
+   view.UserInputLoop();
+     
 }
 
