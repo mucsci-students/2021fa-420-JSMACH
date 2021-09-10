b2a08d1c643a3327eba053098aa36791ad3e736b
manwellaa <63563468+manwellaa@users.noreply.github.com>
2021-09-10 11:33:00 -0400

    Update ConsoleView.h

--- ConsoleView.h
+++ ConsoleView.h
@@ -1,5 +1,12 @@
 #ifndef UML_CONSOLE_VIEW
 #define UML_CONSOLE_VIEW
+#include <string>
+#include "UMLAttribute.h"
+#include "UMLClass.h"
+#include "UMLRelationship.h"
+#include <vector>
+#include <list>
+
 
 /*
     The ConsoleView is responsible for displaying information to the user.
@@ -11,7 +18,26 @@ class ConsoleView
 public:
     ConsoleView(){};
   
-    void UserInputLoop(){};
+    void UserInputLoop(){}  
+    
+    
+    // PrintClass Name will be used to Print the names of the class being passed by the user
+    //@ Para: UMLClass object 
+    void PrintClassName(UMLClass X){}
+    
+    //PrintClass Attribute will Print a List of Attributes which are stored in the specified class
+    //@Para: UMLClass Object  
+    void PrintClassAttribute(UMLClass X){}
+    
+    //PrintClassRelations will Print the current list of Relationships the class being passed has
+    //@para: UmlClass Obj
+    void PrintClassRelations(UMLClass X){}
+    
+    //PrintListofClass will Print out the list of classes currently in the Model
+    //@para: list<string> 
+    void PrintAllClasses(std::list<std::string> x)
+        
+  
   
 };
 
