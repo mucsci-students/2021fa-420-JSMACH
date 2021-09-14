cec8628ddbebd6decb0c8dc7c67bbba92127de48
helittl1 <89500647+helittl1@users.noreply.github.com>
2021-09-14 15:56:57 -0400

    Model implementation (#27)
    * Implemented model stuff related to classes
    
    * Added relationship methods to model
    
    * More model stuff
    
    Co-authored-by: Joshua K Carney <joshuakcarney@gmail.com>
    Co-authored-by: Hunter Little <hlittle@MacBook-Air.local>

--- UMLRelationship.cpp
+++ UMLRelationship.cpp
@@ -1,19 +1,21 @@
 #include "UMLRelationship.h"
+#include "UMLClass.h"
 #include <string>
 
-UMLRelationship::UMLRelationship(std::string name, UMLClass& src, UMLClass& dest){
-  this->RelationshipName=name;
-  this->ClassSrc= src;
-  this->ClassDest=dest;
+UMLRelationship::UMLRelationship(std::string name, UMLClass src, UMLClass dest)
+  : RelationshipName(name), ClassSrc(src), ClassDest (dest)
+{
 }
 
-auto UMLRelationship::get_relationship_name(){
+UMLRelationship::~UMLRelationship(){};
+
+std::string UMLRelationship::get_relationship_name(){
   return this->RelationshipName;
 }
-auto UMLRelationship::get_src_class(){
+UMLClass UMLRelationship::get_src_class(){
   return this->ClassSrc;
 }
-auto UMLRelationship:: get_dest_class(){
+UMLClass UMLRelationship::get_dest_class(){
   return this->ClassDest;
 }
   
