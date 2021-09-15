c7fd6f4fca78c4306cc80202cd5f931da16b6997
Matt Giacoponello <mattgiacoponello@gmail.com>
2021-09-14 22:39:00 -0400

    Implemented UMLController (with view mechanics)

--- UMLController.h
+++ UMLController.h
@@ -9,4 +9,69 @@
     Command parsing may want to be relegated to another class instead of here
     as good practice for separation of concerns.
 */
+
+/***********************************************************/
+//include statements
+
+#include "UMLModel.h"
+#include <iostream>
+#include <vector>
+#include <string.h>
+
+
+/***********************************************************/
+//Using declarations
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::vector;
+
+class UMLController
+{
+    private:
+
+        vector<string> Relships;
+  
+        vector<string> Classes;
+        
+        UMLModel Model;
+        //Helper Prototypes
+
+        bool continuePrompt(string commandLine);
+
+        long unsigned int searchVectorIndex(string input, vector<string> collection);
+
+        bool searchVector(string input, vector<string> collection);
+
+        string sourceParse(string relship);
+
+        string destParse(string relship, long unsigned int index);
+
+        bool search_in_relship(string Class, int option);
+
+    public:
+        UMLController(/* args */);
+        UMLController(UMLModel newModel);
+        ~UMLController();
+        
+
+        void execute();
+
+        void print_command_list();
+
+        void list_classes();
+
+        void listRelationships();
+
+        void createClass();
+
+        void createRelationship();
+
+        void deleteClass();
+
+        void deleteRelationship();
+};
+
+
 #endif
