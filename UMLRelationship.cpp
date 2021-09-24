69dcaa5807422e2e892b22ad4a94e92d29953686
Joshua K Carney <joshuakcarney@gmail.com>
2021-09-24 11:52:06 -0400

    UMLRelationships have a type

--- UMLRelationship.cpp
+++ UMLRelationship.cpp
@@ -4,8 +4,8 @@
 
 UMLRelationship::UMLRelationship(){};
 
-UMLRelationship::UMLRelationship(UMLClass& src, UMLClass& dest)
-  : ClassSrc(&src), ClassDest (&dest)
+UMLRelationship::UMLRelationship(UMLClass& src, UMLClass& dest, RelationshipType type)
+  : ClassSrc(&src), ClassDest (&dest), Type(type)
 {
 }
 
@@ -20,9 +20,31 @@ const UMLClass& UMLRelationship::get_dest_class() const
   return *(this->ClassDest);
 }
 
+std::string UMLRelationship::type_to_string()
+{
+  if(type == RelationshipType::Aggregation)
+  {
+    return "Aggregation";
+  }
+  else if(type == RelationshipType::Composition)
+  {
+    return "Composition";
+  }
+  else if(type == RelationshipType::Inheritance)
+  {
+    return "Inheritance";
+  }
+  else if(type == RelationshipType::Realization)
+  {
+    return "Realization"
+  }
+}
+
 void to_json(json& j, const UMLRelationship& ur)
 {
 	j = json {{"ClassSrc", ur.get_src_class().get_class_name()}, {"ClassDest", ur.get_dest_class().get_class_name()}};
 }
+
+
 
 //THERE IS NO FROM JSON. PAIN.
--- UMLRelationship.h
+++ UMLRelationship.h
@@ -3,22 +3,34 @@
 #define UMLRELATIONSHIP
 
 #include "UMLClass.h"
+#include <string>
 
 #include <nlohmann/json.hpp>
 using json = nlohmann::json;
+
+enum class RelationshipType {
+	Aggregation,
+	Composition,
+	Inheritance,
+	Realization
+};
 
 class UMLRelationship {
 	private:
 		UMLClass* ClassSrc;
 		UMLClass* ClassDest;
+		RelationshipType Type;
 
 	public:
 		UMLRelationship();
-		UMLRelationship(UMLClass& src, UMLClass& dest);
+		UMLRelationship(UMLClass& src, UMLClass& dest, RelationshipType type);
 		~UMLRelationship();
 
 		const UMLClass& get_src_class() const;
 		const UMLClass& get_dest_class() const;
+
+		std::string type_to_string();
+		bool set_type(std::string);
  
 };
 
