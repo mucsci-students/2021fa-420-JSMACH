7fa2f563802711cc2cc5355a7a2d72490f63ff4b
Joshua Carney <48502124+jkcarney@users.noreply.github.com>
2021-09-27 11:43:20 -0400

    Relationship types (#60)
    * UMLRelationships have a type
    
    * Relationships now have a type and can throw exceptions
    
    * added set_type method to relationship
    
    * Removed workflow file that didnt work anyways

--- UMLRelationship.cpp
+++ UMLRelationship.cpp
@@ -1,11 +1,18 @@
 #include "UMLRelationship.h"
 #include "UMLClass.h"
+#include "UMLException.hpp"
 #include <string>
+#include <cstring>
 
 UMLRelationship::UMLRelationship(){};
 
 UMLRelationship::UMLRelationship(UMLClass& src, UMLClass& dest)
   : ClassSrc(&src), ClassDest (&dest)
+{
+}
+
+UMLRelationship::UMLRelationship(UMLClass& src, UMLClass& dest, RelationshipType type)
+  : ClassSrc(&src), ClassDest (&dest), Type(type)
 {
 }
 
@@ -20,9 +27,60 @@ const UMLClass& UMLRelationship::get_dest_class() const
   return *(this->ClassDest);
 }
 
+std::string UMLRelationship::type_to_string()
+{
+  if(Type == RelationshipType::Aggregation)
+  {
+    return "Aggregation";
+  }
+  else if(Type == RelationshipType::Composition)
+  {
+    return "Composition";
+  }
+  else if(Type == RelationshipType::Inheritance)
+  {
+    return "Inheritance";
+  }
+  else if(Type == RelationshipType::Realization)
+  {
+    return "Realization";
+  }
+  return "";
+}
+
+RelationshipType UMLRelationship::type_from_string(std::string typeName)
+{
+  if(strcasecmp(typeName.c_str(), "Aggregation") == 0)
+  {
+    return RelationshipType::Aggregation;
+  }
+  else if(strcasecmp(typeName.c_str(), "Composition") == 0)
+  {
+    return RelationshipType::Composition;
+  }
+  else if(strcasecmp(typeName.c_str(), "Inheritance") == 0)
+  {
+    return RelationshipType::Inheritance;
+  }
+  else if(strcasecmp(typeName.c_str(), "Realization")  == 0)
+  {
+    return RelationshipType::Realization;
+  }
+
+  throw UMLErrorCode::no_such_relationship_type;
+}
+
+void UMLRelationship::set_type(std::string typeName)
+{
+  RelationshipType newType = type_from_string(typeName);
+  this->Type = newType;
+}
+
 void to_json(json& j, const UMLRelationship& ur)
 {
 	j = json {{"ClassSrc", ur.get_src_class().get_class_name()}, {"ClassDest", ur.get_dest_class().get_class_name()}};
 }
+
+
 
 //THERE IS NO FROM JSON. PAIN.
--- UMLRelationship.h
+++ UMLRelationship.h
@@ -3,22 +3,36 @@
 #define UMLRELATIONSHIP
 
 #include "UMLClass.h"
+#include <string>
 
 #include <nlohmann/json.hpp>
 using json = nlohmann::json;
+
+enum class RelationshipType {
+	Aggregation,
+	Composition,
+	Inheritance,
+	Realization
+};
 
 class UMLRelationship {
 	private:
 		UMLClass* ClassSrc;
 		UMLClass* ClassDest;
+		RelationshipType Type;
 
 	public:
 		UMLRelationship();
 		UMLRelationship(UMLClass& src, UMLClass& dest);
+		UMLRelationship(UMLClass& src, UMLClass& dest, RelationshipType type);
 		~UMLRelationship();
 
 		const UMLClass& get_src_class() const;
 		const UMLClass& get_dest_class() const;
+
+		std::string type_to_string();
+		static RelationshipType type_from_string(std::string typeName);
+		void set_type(std::string typeName);
  
 };
 
