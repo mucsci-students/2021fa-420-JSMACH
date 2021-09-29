b7818fa2025517ea55c10aeaa4d5c28a0e842610
manwellaa <63563468+manwellaa@users.noreply.github.com>
2021-09-29 09:20:59 -0700

    test SDL using OPENGL
    sample test window

--- main.cpp
+++ main.cpp
@@ -12,6 +12,15 @@ int main()
     UMLModel model;
     UMLController controller {model};
     controller.execute();
+    
+    
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow("Test with OPENGL",
+    SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, SDL_WINDOW_OPENGL);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    S   DL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GLContext context = SDL_GL_CreateContext(window);
 
     return 0;    
 }
