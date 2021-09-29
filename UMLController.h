7adf91bba1ac326cad64ed4dd22e61c814570786
DragonArchon <89423792+DragonArchon@users.noreply.github.com>
2021-09-29 11:05:49 -0400

    Modified UMLController and ConsoleView (#61)
    Co-authored-by: Matthew T Giacoponello <mtgiacop@csciarch02.muad.local>

--- UMLController.h
+++ UMLController.h
@@ -17,6 +17,9 @@
 #include "UMLRelationship.h"
 #include "UMLClass.h"
 #include "UMLAttribute.h"
+//#include "IView.h"
+#include "ConsoleView.h" //temporary
+
 #include <iostream>
 #include <vector>
 #include <string.h>
@@ -32,55 +35,71 @@ using std::vector;
 
 class UMLController
 {
-    private:
-        UMLModel Model;
-
-        void print_attribute_commands();
-
-        void list_attributes(string className);
-        
-        void add_attribute(string className);
-
-        void delete_attribute(string className);
-
-        void rename_attribute(string className);
-
-
-        // The view will also eventually go here
-        
-    public:
-        UMLController();
-        UMLController(UMLModel newModel);
-        ~UMLController();
+  private:
+    UMLModel Model;
+    //IView Interface; - This will be uncommented once the IView is complete.
     
-        // Takes in user input and calls different functions based on
-        // what command the user typed.
-        void execute();
+    ConsoleView Interface; // This is temporary
 
-        //prints the list of all user commands to be typed in execute()
-        void print_command_list();
+    //void print_attribute_commands();
 
-        //Lists all classes the user has created.
-        void list_classes();
+    void list_attributes(string className);
+        
+    void add_attribute(string className);
 
-        //Lists all relationships the user has created.
-        void list_relationships();
+    void delete_attribute(string className);
 
-        void create_class();
+    void rename_attribute(string className);
 
-        void create_relationship();
 
-        void delete_relationship();
+    // The view will also eventually go here
+        
+  public:
+    UMLController();
+    UMLController(UMLModel newModel);
+    ~UMLController();
+    
+    // Takes in user input and calls different functions based on
+    // what command the user typed.
+    void execute();
 
-        void delete_class();
+    // Returns a string of all available commands for the user to
+    // type in the command line, or click in the GUI.
+    //string command_list();
 
-        void rename_class();
+    // Lists all classes the user has created.
+    void list_classes();
 
-        void edit_attributes();
-  
-        void load_json();
+    // Lists all relationships the user has created.
+    void list_relationships();
 
-        void save_json();
+    // User creates and names a class and may give it any number
+    // of attributes.
+    void create_class();
+
+    // Creates a new relationship between classes.
+    void create_relationship();
+
+    // User will be prompted to type in the class name,
+    // and if it exists, it will be deleted.
+    void delete_class();
+
+    // User will be prompted to type in the source and destination,
+    // and if the relationship exists, it will be deleted.
+    void delete_relationship();
+
+    // User types in the current class name, and then the name
+    // they'd like to change it to. Then it gets renamed.
+    void rename_class();
+
+    // Does various things with attributes based on user input.
+    void edit_attributes();
+
+    // Loads a json save file, overwriting the current session.
+    void load_json();
+
+    // Saves the user's progress into a json file.
+    void save_json();
 
 };
 
