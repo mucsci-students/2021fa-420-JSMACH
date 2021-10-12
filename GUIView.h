bb7f66b186e8c96a6ce92847bc218d9f28f8fab8
Joshua Carney <48502124+jkcarney@users.noreply.github.com>
2021-10-12 13:49:10 -0400

    push to see if this could work on hunters box

--- GUIView.h
+++ GUIView.h
@@ -1,14 +1,6 @@
 #ifndef UML_GUI_VIEW
 #define UML_GUI_VIEW
 
-#include <nana/gui.hpp>
-#include <nana/gui/widgets/button.hpp>
-#include <nana/gui/widgets/textbox.hpp>
-#include <nana/gui/place.hpp>
-#include <nana/gui/widgets/label.hpp>
-#include <nana/gui/widgets/spinbox.hpp>
-#include <nana/gui/widgets/combox.hpp>
-
 #include <string>
 #include <list>
 #include <vector>
@@ -19,115 +11,12 @@
 #include "ClassMethod.h"
 #include "MethodParameter.h"
 
-using namespace nana;
-
 class GUIView {
 	private:
 		UMLModel* Model;
 
 	public:
-		form Form;
-		place CentralWidgets;
-		button Button;
-		label DisplayText;
-		textbox InputText;
-
-		form JSONForm;
-		place JSONWidgets;
-		button JSONButton;
-		textbox JSONInput;
-
-		form ClassWindow;
-		place ClassWidgets;
-		button ClassSubmit;
-		textbox ClassNameText;
-		label ClassNameTextLabel;
-		textbox ClassFields;
-		label ClassFieldsLabel;
-		textbox ClassMethods;
-		label ClassMethodsLabel;
-
-		form RelationshipWindow;
-		place RelationshipWidgets;
-		button RelationshipSubmit;
-		label SrcLabel;
-		textbox RelationshipSrc;
-		label DestLabel;
-		textbox RelationshipDest;
-		combox RelationshipTypeSelect;
-
-		form DeleteClassWindow;
-		place DeleteClassWidgets;
-		button DeleteClassButton;
-		textbox DeleteClassInput;
-
-		form DeleteRelWindow;
-		place DeleteRelWidgets;
-		button DeleteRelButton;
-		textbox DeleteRelSrcInput;
-		textbox DeleteRelDestInput;
-		label DeleteRelSrcLabel;
-		label DeleteRelDestLabel;
-
-		form EditClassWindow;
-		place EditClassWidgets;
-		button EditClassSubmit;
-		textbox EditClassNameText;
-		label EditClassNameTextLabel;
-		textbox EditClassFields;
-		label EditClassFieldsLabel;
-		textbox EditClassMethods;
-		label EditClassMethodsLabel;
-
-		form GetClassWindow;
-		place GetClassWidgets;
-		button GetClassButton;
-		textbox GetClassInput;
-
 		GUIView(UMLModel& model);
-		~GUIView();
-
-		void run();
-		void stop();
-
-		void display_msg(std::string msg);
-
-		void clear_input_text();
-
-		void display_classes();
-
-		std::string single_class_as_string(UMLClass& currentClass);
-		std::string single_relationship_as_string(UMLRelationship& currentRelationship);
-
-		void display_relationships();
-
-		void build_json_window();
-		void display_json_window();
-		void close_json_window();
-
-		void build_class_builder_window();
-		void display_class_builder_window();
-		void close_class_builder_window();
-
-		void build_relationship_window();
-		void display_relationship_window();
-		void close_relationship_window();
-
-		void build_delete_class_window();
-		void display_delete_class_window();
-		void close_delete_class_window();
-
-		void build_delete_relationship_window();
-		void display_delete_relationship_window();
-		void close_delete_relationship_window();
-
-		void build_edit_class_window();
-		void display_edit_class_window(UMLClass& current);
-		void close_edit_class_window();
-
-		void build_get_class_window();
-		void display_get_class_window();
-		void close_get_class_window();
 };
 
 
--- main.cpp
+++ main.cpp
@@ -8,51 +8,70 @@
 #include "GUIController.h"
 #include "UMLRelationship.h"
 
-void run_cli()
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+	#include <wx/wx.h>
+#endif
+
+class MyApp : public wxApp
 {
-    UMLModel model;
-    UMLController controller{ model };
-    controller.execute();
-}
-
-void run_gui()
+public:
+    virtual bool OnInit();
+};
+class MyFrame : public wxFrame
 {
-    // Create model and view
-    // View constructs stuff such as buttons and other UI elements
-    UMLModel model;
-    GUIView view{ model };
-
-    // Construct controller
-    // Controller links view buttons with controller methods
-    GUIController controller{ model, view };
-
-    // Basically just calls view's run function
-    controller.execute();
-}
-
-int main(int argc, char** argv)
+public:
+    MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
+private:
+    void OnHello(wxCommandEvent& event);
+    void OnExit(wxCommandEvent& event);
+    void OnAbout(wxCommandEvent& event);
+    wxDECLARE_EVENT_TABLE();
+};
+enum
 {
+    ID_Hello = 1
+};
+wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
+    EVT_MENU(ID_Hello, MyFrame::OnHello)
+    EVT_MENU(wxID_EXIT, MyFrame::OnExit)
+    EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
+wxEND_EVENT_TABLE()
+wxIMPLEMENT_APP(MyApp);
 
-    if (argc > 1)
-    {
-        if (string(argv[1]) == "--cli")
-        {
-            run_cli();
-        }
-        else if (string(argv[1]) == "--gui")
-        {
-            run_gui();
-        }
-        else
-        {
-            std::cout << "Unknown UI flag. Use either --cli or --gui" << std::endl;
-        }
-    }
-    else
-    {
-        run_gui();
-    }
-
-    return 0;
+bool MyApp::OnInit()
+{
+    MyFrame* frame = new MyFrame("Hello World", wxPoint(50, 50), wxSize(450, 340));
+    frame->Show(true);
+    return true;
 }
-
+MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
+    : wxFrame(NULL, wxID_ANY, title, pos, size)
+{
+    wxMenu* menuFile = new wxMenu;
+    menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
+        "Help string shown in status bar for this menu item");
+    menuFile->AppendSeparator();
+    menuFile->Append(wxID_EXIT);
+    wxMenu* menuHelp = new wxMenu;
+    menuHelp->Append(wxID_ABOUT);
+    wxMenuBar* menuBar = new wxMenuBar;
+    menuBar->Append(menuFile, "&File");
+    menuBar->Append(menuHelp, "&Help");
+    SetMenuBar(menuBar);
+    CreateStatusBar();
+    SetStatusText("Welcome to wxWidgets!");
+}
+void MyFrame::OnExit(wxCommandEvent& event)
+{
+    Close(true);
+}
+void MyFrame::OnAbout(wxCommandEvent& event)
+{
+    wxMessageBox("This is a wxWidgets' Hello world sample",
+        "About Hello World", wxOK | wxICON_INFORMATION);
+}
+void MyFrame::OnHello(wxCommandEvent& event)
+{
+    wxLogMessage("Hello world from wxWidgets!");
+}
